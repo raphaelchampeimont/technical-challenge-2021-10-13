49380bcd39a3084b9e38801465a7bb2d3ef2b504
Raphael Champeimont <raphael.champeimont@almacha.org>
2021-10-13 17:53:07 +0200

    basic URL format checking in downloadLink

--- app/api/download.ts
+++ app/api/download.ts
@@ -1,10 +1,37 @@
 import { Request, Response, NextFunction } from "express";
 import { Document } from "../common/db";
+import { ValidationError } from "sequelize";
 
 export async function downloadLink(req: Request, res: Response) {
-  await Document.create({
-    originalUrl: req.body.url,
-  });
+  if (typeof req.body.url != "string") {
+    return res.status(400).json({
+      error: "INVALID_REQUEST_FORMAT",
+    });
+  }
+  if (!req.body.url.toLowerCase().endsWith(".pdf")) {
+    return res.status(400).json({
+      error: "INVALID_URL_EXTENSION",
+    });
+  }
+  try {
+    await Document.create({
+      originalUrl: req.body.url,
+    });
+  } catch (error) {
+    if (
+      error instanceof ValidationError &&
+      error.name == "SequelizeUniqueConstraintError"
+    ) {
+      return res.json({
+        status: "URL_ALREADY_SUBMITTED",
+      });
+    } else {
+      // Re-throw if other error
+      throw error;
+    }
+  }
 
-  await res.json({});
+  res.json({
+    status: "URL_SUCCESSFULLY_SUBMITTED",
+  });
 }
