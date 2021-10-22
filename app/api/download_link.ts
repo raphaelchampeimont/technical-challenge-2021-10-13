062c2a58289200a908d334746bb0baaaa0a2315f
Raphael Champeimont <raphael.champeimont@almacha.org>
2021-10-22 11:28:25 +0200

    Add typing to Download Link API responses

--- app/api/download_link.ts
+++ app/api/download_link.ts
@@ -2,16 +2,52 @@ import { Request, Response, NextFunction } from "express";
 import { Document } from "../common/db";
 import { ValidationError } from "sequelize";
 
+type DownloadLinkOkStatusCode =
+  | "URL_ALREADY_SUBMITTED"
+  | "URL_SUCCESSFULLY_SUBMITTED";
+
+interface DownloadLinkSuccessfulResponse {
+  status: DownloadLinkOkStatusCode;
+}
+
+type DownloadLinkErrorCode = "INVALID_REQUEST_FORMAT" | "INVALID_URL_EXTENSION";
+
+interface DownloadLinkFailedResponse {
+  error: DownloadLinkErrorCode;
+}
+
+type DownloadLinkAPIResponse =
+  | DownloadLinkFailedResponse
+  | DownloadLinkSuccessfulResponse;
+
+function respondWithClientError(
+  res: Response,
+  error_code: DownloadLinkErrorCode
+) {
+  respondWithJson(res.status(400), {
+    error: error_code,
+  });
+}
+
+function respondWithOkStatus(
+  res: Response,
+  error_code: DownloadLinkOkStatusCode
+) {
+  respondWithJson(res, {
+    status: error_code,
+  });
+}
+
+function respondWithJson(res: Response, json: DownloadLinkAPIResponse) {
+  res.json(json);
+}
+
 export async function downloadLink(req: Request, res: Response) {
   if (typeof req.body.url != "string") {
-    return res.status(400).json({
-      error: "INVALID_REQUEST_FORMAT",
-    });
+    return respondWithClientError(res, "INVALID_REQUEST_FORMAT");
   }
   if (!req.body.url.toLowerCase().endsWith(".pdf")) {
-    return res.status(400).json({
-      error: "INVALID_URL_EXTENSION",
-    });
+    return respondWithClientError(res, "INVALID_URL_EXTENSION");
   }
   try {
     await Document.create({
@@ -22,16 +58,12 @@ export async function downloadLink(req: Request, res: Response) {
       error instanceof ValidationError &&
       error.name == "SequelizeUniqueConstraintError"
     ) {
-      return res.json({
-        status: "URL_ALREADY_SUBMITTED",
-      });
+      return respondWithOkStatus(res, "URL_ALREADY_SUBMITTED");
     } else {
       // Re-throw if other error
       throw error;
     }
   }
 
-  res.json({
-    status: "URL_SUCCESSFULLY_SUBMITTED",
-  });
+  return respondWithOkStatus(res, "URL_SUCCESSFULLY_SUBMITTED");
 }
